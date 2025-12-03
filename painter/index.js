c885a9e7e0e5cb169471cc6e3170d3fb7681ae4e
sebrai <sebbranden@gmail.com>
2025-12-03 13:54:48 +0100

    added a stroke width selection and ui indication as to color and stroke width

--- painter/index.js
+++ painter/index.js
@@ -2,6 +2,7 @@ const targetElement = document.getElementById('mpos');
 const c = document.getElementById("c")
 const ctx = c.getContext("2d")
 const paintcolors = document.getElementById("colors")
+const paintbsize =document.getElementById("strokesize")
 let isPointerDown = false
 let currcolor = "black"
 document.addEventListener('mousemove', (event) => {
@@ -62,8 +63,28 @@ document.addEventListener('pointerup', function (event) {
 for (let childen = 0; childen < paintcolors.children.length; childen++) {
     const element = paintcolors.children[childen];
     element.style.color = element.textContent
-    console.log(element)
+    // console.log(element)
     element.addEventListener("click",()=>{
         ctx.strokeStyle = element.textContent
+        for (let r = 0; r < paintcolors.children.length; r++) {
+            const rm = paintcolors.children[r]
+            rm.style.boxShadow = "none"
+        }
+        element.style.boxShadow = "1px 1px 5px 0 inset"
+        // console.log(element.style.boxShadow)
     })
+}
+console.log(paintbsize.children)
+for (let children = 0; children < paintbsize.children.length; children++) {
+    const element = paintbsize.children[children];
+    element.addEventListener("click",()=>{
+        ctx.lineWidth = parseInt(element.textContent,10)
+         for (let r = 0; r < paintbsize.children.length; r++) {
+            const rm = paintbsize.children[r]
+            rm.style.boxShadow = "none"
+        }
+        element.style.boxShadow = "1px 1px 2px 0 inset"
+        // console.log(element.style.boxShadow)
+    })
+    
 }
