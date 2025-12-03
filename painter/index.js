e1c7f901b5dfea24af9e858b3e5a0aab1a85b148
sebrai <sebbranden@gmail.com>
2025-12-03 12:54:12 +0100

    added a color selection to painter

--- painter/index.js
+++ painter/index.js
@@ -1,46 +1,69 @@
 const targetElement = document.getElementById('mpos');
 const c = document.getElementById("c")
-const ctx = c.getContext("2d");
-let isPointerDown = false;
+const ctx = c.getContext("2d")
+const paintcolors = document.getElementById("colors")
+let isPointerDown = false
+let currcolor = "black"
 document.addEventListener('mousemove', (event) => {
-    const elementRect = c.getBoundingClientRect();
+    const elementRect = c.getBoundingClientRect()
 
-    const relativeX = event.clientX - elementRect.left;
-    const relativeY = event.clientY - elementRect.top;
+    const relativeX = event.clientX - elementRect.left
+    const relativeY = event.clientY - elementRect.top
 
     targetElement.textContent = `Mouse position relative to element: X=${relativeX}, Y=${relativeY}`
 });
 
 c.addEventListener("pointermove", (event) => {
     if (isPointerDown) {
-        ctx.beginPath();
-        const elementRect = c.getBoundingClientRect();
+       
+        const elementRect = c.getBoundingClientRect()
 
-        const relativeX = event.clientX - elementRect.left;
-        const relativeY = event.clientY - elementRect.top;
-        ctx.arc(relativeX, relativeY, 1, 0, 2 * Math.PI);
-        ctx.stroke();
+        const relativeX = event.clientX - elementRect.left
+        const relativeY = event.clientY - elementRect.top
+        ctx.arc(relativeX, relativeY, 10**-32, 0, 2 * Math.PI)
+        ctx.stroke()
         console.log(`Mouse position relative to element: X=${relativeX}, Y=${relativeY}`)
     }
 
 
 })
+// c.addEventListener("onclick", (event) => {
+   
+//         ctx.beginPath();
+//         const elementRect = c.getBoundingClientRect();
 
+//         const relativeX = event.clientX - elementRect.left;
+//         const relativeY = event.clientY - elementRect.top;
+//         ctx.arc(relativeX, relativeY, 1, 0, 2 * Math.PI);
+//         ctx.stroke();
+//         console.log(`Mouse position relative to element: X=${relativeX}, Y=${relativeY}`)
+    
+
+
+// })
 
 
 document.addEventListener('pointerdown', function (event) {
 
     if (event.isPrimary) {
-        isPointerDown = true;
-
+        isPointerDown = true
+         ctx.beginPath()
     }
 });
 
 document.addEventListener('pointerup', function (event) {
     if (event.isPrimary) {
-        isPointerDown = false;
+        isPointerDown = false
 
     }
 });
 
 
+for (let childen = 0; childen < paintcolors.children.length; childen++) {
+    const element = paintcolors.children[childen];
+    element.style.color = element.textContent
+    console.log(element)
+    element.addEventListener("click",()=>{
+        ctx.strokeStyle = element.textContent
+    })
+}
